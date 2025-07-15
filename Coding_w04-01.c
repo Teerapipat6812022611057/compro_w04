2c515b21f8c7eb3beb5a433089db69008e33b756
Teerapipat6812022611057 <s6812022611057@email.kmutnb.ac.th>
2025-07-15 17:54:34 +0700

    compro_w04 bykim

---
+++ Coding_w04-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){       //keywords คือ int
+    printf("Hello,\n");
+    printf("c Token!\n");
+    return 0;
+}
---
+++ Coding_w04-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {   //keywords คือ int   
+    int a = 10;
+    printf("a = %d" , a);
+    return 0;
+}
